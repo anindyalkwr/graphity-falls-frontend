af3b1b5481669533324c05d3b0205f89b85ad998
Anindya Lokeswara <anindya.lokeswara@ui.ac.id>
2024-10-03 00:24:19 +0700

    Prettier configuration

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+const Custom404 = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-xl text-muted-foreground">
+                404 - Page Not Found
+            </h1>
+        </div>
+    )
+}
+export default Custom404
