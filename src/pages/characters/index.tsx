6a2e17de254c36f5e8e0310c995748e3bb380c34
Anindya Lokeswara <anindya.lokeswara@ui.ac.id>
2024-10-31 22:30:08 +0700

    fix build issues

--- src/pages/characters/index.tsx
+++ src/pages/characters/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import characters from './constant'
+import characters from '@/data/characters'
 
 const Characters = () => {
     return (
